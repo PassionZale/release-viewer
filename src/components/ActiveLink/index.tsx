31326c3dc0b9ebf25ecad9ed28aa9dbc24b4db07
Zhang Lei <372470745@qq.com>
2024-08-19 16:57:13 +0800

    N/A

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -14,7 +14,6 @@ type ActiveLinkProps = LinkProps & {
   activeClassName: string;
 };
 
-// eslint-disable-next-line react/display-name
 const ActiveLink = forwardRef<
   HTMLAnchorElement,
   PropsWithChildren<ActiveLinkProps>
@@ -58,49 +57,6 @@ const ActiveLink = forwardRef<
   );
 });
 
-// const ActiveLink = ({
-//   children,
-//   activeClassName,
-//   className,
-//   ...props
-// }: PropsWithChildren<ActiveLinkProps>) => {
-//   const pathname = usePathname();
-//   const [computedClassName, setComputedClassName] = useState(className);
-
-//   useEffect(() => {
-//     if (pathname) {
-//       const linkUrl = getLinkUrl(props.href, props.as);
-
-//       const linkPathname = new URL(linkUrl, location.href).pathname;
-//       const activePathname = new URL(pathname, location.href).pathname;
-
-//       const isActived =
-//         linkPathname === "/admin"
-//           ? activePathname === linkPathname
-//           : activePathname.startsWith(linkPathname);
-
-//       const newClassName = isActived
-//         ? `${className} ${activeClassName}`.trim()
-//         : className;
-
-//       if (newClassName !== computedClassName) {
-//         setComputedClassName(newClassName);
-//       }
-//     }
-//   }, [
-//     pathname,
-//     props.as,
-//     props.href,
-//     activeClassName,
-//     className,
-//     computedClassName,
-//   ]);
-
-//   return (
-//     <Link className={computedClassName} {...props}>
-//       {children}
-//     </Link>
-//   );
-// };
+ActiveLink.displayName = "ActiveLink";
 
 export default ActiveLink;
