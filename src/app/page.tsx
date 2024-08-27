68710c1322944600a88a535d22c73f18355fb911
Zhang Lei <372470745@qq.com>
2024-08-27 17:35:29 +0800

    config: mysql

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,10 @@ export default function Home() {
             </div>
 
             <div className="mt-10 sm:mt-20">
-              <button className="p-[3px] relative">
+              <button
+                className="p-[3px] relative"
+                onClick={() => window.open("/admin/release/create")}
+              >
                 <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
                 <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent break-keep	">
                   发布应用
