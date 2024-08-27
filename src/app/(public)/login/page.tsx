41bc6ca7dcd389b65333dc0f95cd138d00adf4d2
Zhang Lei <372470745@qq.com>
2024-08-27 17:52:44 +0800

    N/A

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import LoginForm from "./form";
-import { Suspense } from "react"
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "登录",
@@ -46,9 +46,9 @@ export default function Page() {
             </p>
           </div>
 
-					<Suspense>
-	          <LoginForm />
-					</Suspense>
+          <Suspense>
+            <LoginForm />
+          </Suspense>
 
           <p className="px-8 text-center text-sm text-muted-foreground">
             为了便于演示，自动使用访客账号（只读）填充了用户名和密码，你可以直接使用它登录。
