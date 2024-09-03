0fa96857dadb8b2d20c9bc0f3ef53e537e03a443
Zhang Lei <372470745@qq.com>
2024-09-03 14:44:40 +0800

    refactor: Profile

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import LoginForm from "./form";
 import { Suspense } from "react";
+import { IconGhost2 } from "@tabler/icons-react";
 
 export const metadata: Metadata = {
   title: "登录",
@@ -12,28 +13,15 @@ export default function Page() {
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Logo
+          <IconGhost2 className="mr-2 h-6 w-6" />
+          Release Viewer
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
+              &ldquo;基于 `Nextjs` 所实现的微平台，用于预览所有应用的发布日志。&rdquo;
             </p>
-            <footer className="text-sm">Sofia Davis</footer>
+            <footer className="text-sm">Lei Zhang</footer>
           </blockquote>
         </div>
       </div>
--- src/app/api/(public)/auth/signout/route.ts
+++ src/app/api/(public)/auth/signout/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  const response = NextResponse.redirect(new URL(`/logout`, request.url));
+  const response = NextResponse.redirect(new URL(`/login`, request.url));
 
   response.cookies.delete(COOKIE_JWT_KEY);
 
