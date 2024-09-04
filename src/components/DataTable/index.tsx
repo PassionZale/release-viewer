d33137993a31815242793377b5befa554f6ffdb6
Zhang Lei <372470745@qq.com>
2024-09-04 10:36:12 +0800

    fix: data-table loading

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -195,7 +195,7 @@ export function DataTable<TData>({
                 </TableRow>
               ))}
             </TableHeader>
-            <TableBody>
+            <TableBody className="relative">
               {table.getRowModel().rows?.length ? (
                 table.getRowModel().rows.map((row) => (
                   <TableRow
@@ -218,13 +218,15 @@ export function DataTable<TData>({
                     colSpan={columns.length}
                     className="h-24 text-center"
                   >
-                    {loading ? (
-                      <IconLoader2 className={"h-6 w-6 animate-spin mx-auto"} />
-                    ) : (
-                      "暂无数据"
-                    )}
+                    暂无数据
                   </TableCell>
                 </TableRow>
+              )}
+
+              {loading && (
+                <div className="absolute h-24 top-24 left-0 right-0 text-center">
+                  <IconLoader2 className={"h-6 w-6 animate-spin mx-auto"} />
+                </div>
               )}
             </TableBody>
           </Table>
