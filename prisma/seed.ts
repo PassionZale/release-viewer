73c5c6976449034c4c24402ced99e8b4fce6b167
Zhang Lei <372470745@qq.com>
2024-09-09 14:31:26 +0800

    doc: env docs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { Role } from "../src/types/enum";
-import { encrypt } from "../src/libs/bcrypt";
 
 const prisma = new PrismaClient();
 
 const systems = [
   {
     label: "中台",
-    value: "zhongtai",
+    value: "middle-platform",
   },
   {
     label: "C端",
@@ -42,51 +40,21 @@ const platforms = [
   },
 ];
 
-// TODO fix 这里的 process.env 没有生效！！
-const users = [
-  // admin user
-  {
-    nickname: "admin",
-    username: process.env.DASHBOARD_ADMIN_USERNAME || "admin",
-    hashedPassword: encrypt(process.env.DASHBOARD_ADMIN_PASSWORD || "admin"),
-    role: Role.ADMIN,
-  },
-  // api user
-  {
-    nickname: "API",
-    username: "API",
-    hashedPassword: "Special API user and can never be logged in :)",
-    role: Role.DEVELOPER,
-  },
-  // visitor user
-  {
-    nickname: "visitor",
-    username: "visitor",
-    hashedPassword: encrypt("visitor"),
-    role: Role.VISITOR,
-  },
-];
-
 async function main() {
   console.log(`Start seeding ...`);
-
-  for (const u of users) {
-    const user = await prisma.user.create({ data: u });
-    console.log(`Created user with id: ${user.id}`);
-  }
 
   for (const s of systems) {
     const system = await prisma.system.create({
       data: s,
     });
-    console.log(`Created system with id: ${system.id}`);
+    console.log(`Created system: ${JSON.stringify(system)}`);
   }
 
   for (const p of platforms) {
     const platform = await prisma.platform.create({
       data: p,
     });
-    console.log(`Created platform with id: ${platform.id}`);
+    console.log(`Created platform: ${JSON.stringify(platform)}`);
   }
 
   console.log(`Seeding finished.`);
